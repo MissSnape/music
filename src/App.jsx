05ef0ed2e7ec4dd03345f48be83d993dea3f0231
Анна Г <125292031+MissSnape@users.noreply.github.com>
2023-09-26 14:02:19 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './style.css';
-import { Search } from './components/Search';
-import { Filter } from './components/Filter';
-import { TrackListHeader } from './components/TrackListHeader';
-import { TrackList } from './components/TrackList';
+import React from 'react';
+import './App.css';
+import { NavMenu } from './components/NavMenu';
+import { TracsPlayer } from './components/TracsPlayer';
 import { SideBar } from './components/SideBar';
-import { User } from './components/User';
-import { Player } from './components/Player';
-import { BurgerMenu } from './components/BurgerMenu';
-
+import { Filters } from './components/filters';
+import { Search } from './components/Search';
+import { TrackListHeader } from './components/HeaderTrackList';
+import { User } from './components/users';
+import { NavBurger } from './components/NavBurger';
+import { TracsList } from './components/TracsList';
 function App() {
   return (
     <div className="wrapper">
@@ -17,15 +18,18 @@ function App() {
             <div className="nav__logo logo">
               <img className="logo__image" src="img/logo.png" alt="logo"></img>
             </div>
-            <BurgerMenu />
+            <NavBurger/>
+            <NavMenu />
+           
           </nav>
           <div className="main__centerblock centerblock">
             <Search />
             <h2 className="centerblock__h2">Треки</h2>
-            <Filter />
+            <Filters/>
             <div className="centerblock__content">
               <TrackListHeader />
-              <TrackList />
+              <TracsList />
+              
             </div>
           </div>
           <div className="main__sidebar sidebar">
@@ -34,12 +38,11 @@ function App() {
           </div>
         </main>
         <div className="bar">
-          <Player />
+          <TracsPlayer/>
         </div>
         <footer className="footer"></footer>
       </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/NavBurger.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function NavBurger(){
+    return(
+        <div className="nav__burger burger">
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+      </div>
+    )
+}
+export {NavBurger};
